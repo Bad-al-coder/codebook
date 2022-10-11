da59ef5fa0aa7daa184539937c048e54b763dc0f
Badal Sharma <113523283+Bad-al-coder@users.noreply.github.com>
2022-10-11 14:12:11 +0530

    Advance_pattern programs
    includes butterfly, solid rhombus, hollow rhombus, number pyramid, palindromic pattern, diamond, z_pattern

---
+++ advpatter.java
@@ -0,0 +1,135 @@
+public class advpatter {
+    public static void main(String args[]){
+        // Butterfly pattern
+        int n=3;
+        for (int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            
+            for(int j=1; j<= 2*(n-i); j++){
+                System.out.print(" ");
+            } 
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    
+
+        for (int i=n; i>=1; i--){
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            
+            for(int j=1; j<= 2*(n-i); j++){
+                System.out.print(" ");
+            } 
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    
+
+
+        // solid rhombus
+        // int n=5;
+        // for(int i=1; i<=n; i++){
+        //     for(int j=1; j<=n-i; j++){
+        //         System.out.print(" ");
+
+        //     }
+        //     for(int j=1; j<=n; j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+        // hollow rhombus
+        // int n=5;
+        // for(int i=1; i<=n; i++){
+        //     for(int j=1; j<=n-i; j++){
+        //         System.out.print(" ");
+
+        //     }
+        //     for(int j=1; j<=n; j++){
+        //         if(i==1 || i==n || j==1 || j==n){
+        //         System.out.print("*");
+        //         }
+        //         else{
+        //             System.out.print(" ");
+        //         }
+        //     }
+        //     System.out.println();
+        // }
+
+        // number pyramid
+    //     int n=5;
+     
+    //     for(int i=1; i<=n; i++){
+    //         for(int j=1; j<=n-i;j++){
+    //             System.out.print(" ");
+
+    //         }
+    //         for(int j=1;j<=i; j++){
+    //         System.out.print(i+" ");
+            
+    //     }
+    //     System.out.println();
+        
+    // }
+
+
+    // palindromic pattern
+    // int n=5;
+    // for(int i=1; i<=n; i++){
+    //     for(int j=1;j<=n-i; j++){
+    //         System.out.print(" ");
+    //     }
+    //     for(int j=i; j>=1; j--){
+    //         System.out.print(j);
+    //     }
+    //     for(int j=2; j<=i; j++){
+    //         System.out.print(j);
+    //     }
+    //     System.out.println();
+    // }
+
+
+    // Diamond pattern
+    // int n=4;
+    // for(int i=1; i<=n; i++){
+    //     // for spaces
+    //     for(int j=1;j<=n-i; j++){
+    //         System.out.print(" ");
+    //     }
+    //     // for upper half
+    //     for(int j=1; j<=(2*i)-1; j++){
+    //         System.out.print("*");
+    //     }
+        
+    //     System.out.println();
+    // }
+    // for(int i=n; i>=1; i--){
+    //     // for spaces
+    //     for(int j=1;j<=n-i; j++){
+    //         System.out.print(" ");
+    //     }
+    //     // for upper half
+    //     for(int j=1; j<=(2*i)-1; j++){
+    //         System.out.print("*");
+    //     }
+        
+    //     System.out.println();
+    // }
+    // }
+
+
+
+
+
+
+    
+}
+}
