6c076fff8eca558501352f5f8a175f628f25cdf5
Badal Sharma <113523283+Bad-al-coder@users.noreply.github.com>
2022-10-11 14:18:48 +0530

    Update advpatter.java
    includes butterfly, solid + hollow rhombus, number pyramid, palindromic pattern, diamond & z_pattern

--- advpatter.java
+++ advpatter.java
@@ -1,35 +1,35 @@
 public class advpatter {
     public static void main(String args[]){
         // Butterfly pattern
-        int n=3;
-        for (int i=1; i<=n; i++){
-            for(int j=1; j<=i; j++){
-                System.out.print("*");
-            }
+//         int n=3;
+//         for (int i=1; i<=n; i++){
+//             for(int j=1; j<=i; j++){
+//                 System.out.print("*");
+//             }
             
-            for(int j=1; j<= 2*(n-i); j++){
-                System.out.print(" ");
-            } 
-            for(int j=1; j<=i; j++){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
+//             for(int j=1; j<= 2*(n-i); j++){
+//                 System.out.print(" ");
+//             } 
+//             for(int j=1; j<=i; j++){
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
     
 
-        for (int i=n; i>=1; i--){
-            for(int j=1; j<=i; j++){
-                System.out.print("*");
-            }
+//         for (int i=n; i>=1; i--){
+//             for(int j=1; j<=i; j++){
+//                 System.out.print("*");
+//             }
             
-            for(int j=1; j<= 2*(n-i); j++){
-                System.out.print(" ");
-            } 
-            for(int j=1; j<=i; j++){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
+//             for(int j=1; j<= 2*(n-i); j++){
+//                 System.out.print(" ");
+//             } 
+//             for(int j=1; j<=i; j++){
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
     
 
 
