b8481103d7dbd69b9cf7192d2d9bfe57d0c1f4ed
Badal Sharma <113523283+Bad-al-coder@users.noreply.github.com>
2022-10-11 14:15:35 +0530

    Update advpatter.java

--- advpatter.java
+++ advpatter.java
@@ -124,6 +124,27 @@ public class advpatter {
     //     System.out.println();
     // }
     // }
+        
+    //z_pattern program
+//         public class z_pattern {
+//     public static void main(String args[]){
+//         int n=6;
+        
+//         for(int i=1; i<=n; i++){
+//             for(int j=1;j<=n; j++ ){
+//                 if( i+j==n+1||i==1||i==n){
+//                 System.out.print("* ");
+//                 }else{
+//                     System.out.print("  ");
+//                 }
+
+//             }
+//             System.out.println();
+
+//         }
+//     }
+// }
+
 
 
 
